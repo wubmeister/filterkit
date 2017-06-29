6271b85f99ca771fc1144e2aca9f5ee7787bcf53
Wubbo Bos <wubbo@addnoise.nl>
2017-06-29 14:36:08 +0200

    Add jQuery wrapper for the dropdown

--- src/jquery.js
+++ src/jquery.js
@@ -5,4 +5,39 @@ if ('$' in window) {
         });
         return this;
     };
+
+    $.fn.fkDropdown = function (options) {
+        this.each(function () {
+            var $dd;
+
+            options = FilterKit.resolveOptions(options, {
+                allowAdditions: false,
+                message: {
+                    addResult: 'Add <strong>{term}</strong>',
+                    noResults: 'No results found'
+                }
+            });
+
+            if (this.nodeName == 'SELECT') {
+                $dd = $('<div class="fk-dropdown"></div>').prependTo(this).append(this);
+            } else {
+                $dd = $(this);
+            }
+
+            if ($dd.find('.items').length == 0) {
+                $dd.append('<div class="items"></div>');
+            }
+            if ($dd.find('.items .noresults').length == 0) {
+                $dd.find('.items').append('<div class="noresults item">' + options.message.noResults + '</div>');
+            }
+
+            if (options.allowAdditions && $dd.find('.items .additem').length == 0) {
+                $dd.find('.items').append('<div class="additem item">' + options.message.addResult + '</div>');
+            }
+
+            FilterKit.Util.SelectionDropdown($dd[0], options);
+        });
+
+        return this;
+    }
 }
